b78de9cfb3f1527856c80e542558016ec8f68ef0
grime3ja <grime3ja@dukes.jmu.edu>
2024-03-26 15:31:46 -0400

    fixed form js

--- scripts/createcard.js
+++ scripts/createcard.js
@@ -1,9 +1,9 @@
 let previewBtn = document.getElementById("preview").addEventListener("click", function() {
-    let title = document.querySelector("#title");
-    let subtitle = document.querySelector("#subtitle");
-    let recipient = document.querySelector("#to");
-    let message = document.querySelector("#message");
-    let sender = document.querySelector("#from");
+    let title = document.querySelector("#title").value;
+    let subtitle = document.querySelector("#subtitle").value;
+    let recipient = document.querySelector("#to").value;
+    let message = document.querySelector("#message").value;
+    let sender = document.querySelector("#from").value;
     console.log(title + subtitle + recipient + message + sender);
 });
 let submitBtn = document.getElementById("submit");
