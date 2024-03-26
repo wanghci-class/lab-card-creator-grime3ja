915059d00ff680ccc4b8c81d4c05570dd69bc6e8
grime3ja <grime3ja@dukes.jmu.edu>
2024-03-26 15:57:05 -0400

    initial attempt for editing the letter

--- scripts/createcard.js
+++ scripts/createcard.js
@@ -1,9 +1,15 @@
-let previewBtn = document.getElementById("preview").addEventListener("click", function() {
+document.getElementById("preview").addEventListener("click", function() {
     let title = document.querySelector("#title").value;
     let subtitle = document.querySelector("#subtitle").value;
     let recipient = document.querySelector("#to").value;
     let message = document.querySelector("#message").value;
     let sender = document.querySelector("#from").value;
-    console.log(title + subtitle + recipient + message + sender);
+    
+    document.querySelector("#title-text").textContent = title;
+    document.querySelector("subtitle-text").textContent = subtitle;
+
+    document.querySelector("#to-text").textContent = recipient;
+    document.querySelector("#message-text").textContent = message;
+    document.querySelector("#from-text").textContent = sender;
 });
 let submitBtn = document.getElementById("submit");
